e03fbb3d301a484c3a568452c1dfd40cacb3bad3
akapochi <redstar.ihsohaka@gmail.com>
2021-10-08 16:02:29 +0900

    format

--- src/application/entity/field.ts
+++ src/application/entity/field.ts
@@ -1,8 +1,8 @@
 export class Field {
-  public length: number;
-  public square: number[][];
-   constructor(square: number[][]) {
-      this.square = square;
-      this.length = 1;
-   }
+    public length: number;
+    public square: number[][];
+    constructor(square: number[][]) {
+        this.square = square;
+        this.length = 1;
+    }
 }
--- src/application/entity/player.ts
+++ src/application/entity/player.ts
@@ -1,20 +1,20 @@
-type PlayerStatus = "alive" | "dead";
+type PlayerStatus = 'alive' | 'dead';
 
-import { User } from "./user"
+import { User } from './user';
 
-const user = new User("username");
+const user = new User('username');
 
 export class Player {
-  public position: { x: number, y: number };
-  public point: number;
-  public uid: string;
-  public username: string;
-  public status: PlayerStatus;
-  constructor() {
-    this.position = { x: 0, y: 0 }
-    this.point = 0;
-    this.uid = user.uid;
-    this.username = user.uid;
-    this.status = "alive";
-  }
+    public position: { x: number; y: number };
+    public point: number;
+    public uid: string;
+    public username: string;
+    public status: PlayerStatus;
+    constructor() {
+        this.position = { x: 0, y: 0 };
+        this.point = 0;
+        this.uid = user.uid;
+        this.username = user.uid;
+        this.status = 'alive';
+    }
 }
