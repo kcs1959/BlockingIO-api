0fc62e6ba5683d70b1fc6c3783889d491446c1c2
akapochi <redstar.ihsohaka@gmail.com>
2021-10-08 16:02:29 +0900

    format

--- src/application/entity/npc.ts
+++ src/application/entity/npc.ts
@@ -1,8 +1,8 @@
 export class Npc {
-  public position: { x: number, y: number };
-  public name: string;
-  constructor(name: string) {
-    this.position = { x: 0, y: 0 };
-    this.name = name;
-  }
+    public position: { x: number; y: number };
+    public name: string;
+    constructor(name: string) {
+        this.position = { x: 0, y: 0 };
+        this.name = name;
+    }
 }
--- src/application/entity/user.ts
+++ src/application/entity/user.ts
@@ -1,12 +1,12 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
 export class User {
-  public username: string;
-  public point: number;
-  public uid: string;
-  constructor(username: string) {
-    this.username = username;
-    this.point = 0;
-    this.uid = uuidv4();
-  }
+    public username: string;
+    public point: number;
+    public uid: string;
+    constructor(username: string) {
+        this.username = username;
+        this.point = 0;
+        this.uid = uuidv4();
+    }
 }
