8d61c0550986eecc2ac645be002b9bf331dfab04
速川湧気 <hykwyuk0125@gmail.com>
2021-10-10 00:54:28 +0900

    change table name

--- migration/migration.sql
+++ migration/migration.sql
@@ -1,7 +1,7 @@
-CREATE TABLE UserData (       
-    user_id TEXT,
+CREATE TABLE Users (       
+    id TEXT,
     user_name TEXT NOT NULL,
     password TEXT NOT NULL,
     point INTEGER NOT NULL DEFAULT 0,
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (id)
 );
