ac5ef3aa63c9ce524b3888ef45ff0fcdff0fa04c
akapochi <redstar.ihsohaka@gmail.com>
2021-10-09 19:20:30 +0900

    reviewに応じて修正

--- src/application/entity/field.ts
+++ src/application/entity/field.ts
@@ -1,8 +1,23 @@
 export class Field {
     public length: number;
     public square: number[][];
-    constructor(square: number[][]) {
-        this.square = square;
+    constructor(length: number) {
+        const square = new Array(length);
+        for (let i = 0; i < length; i++) {
+            square[i] = new Array(length).fill(0);
+        }
+
+        /*
+        const square2 = (length: number): number[][] => {
+            return Array.from(new Array(length), _ => new Array(length).fill(0));
+        };
+
+        const square3 = (length: number): number[][] => {
+            return [...Array(length)].map(() => Array(length).fill(0));
+        };
+        */
+
+        this.square = square
         this.length = 1;
     }
 }
--- src/application/entity/npc.ts
+++ src/application/entity/npc.ts
@@ -1,8 +1,10 @@
+import { Position } from './position';
+
 export class Npc {
-    public position: { x: number; y: number };
+    public position: Position;
     public name: string;
-    constructor(name: string) {
-        this.position = { x: 0, y: 0 };
+    constructor(name: string, position: Position) {
+        this.position = position;
         this.name = name;
     }
 }
