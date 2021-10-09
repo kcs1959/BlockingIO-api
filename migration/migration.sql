2fc81e9fec04a14c172cfec36ab40bb826209eec
速川湧気 <hykwyuk0125@gmail.com>
2021-10-10 00:32:08 +0900

    fix create table

--- migration/migration.sql
+++ migration/migration.sql
@@ -1,7 +1,7 @@
-CREATE TABLE User (       
+CREATE TABLE UserData (       
     user_id TEXT,
     user_name TEXT NOT NULL,
     password TEXT NOT NULL,
     point INTEGER NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
+    PRIMARY KEY (user_id)
 );
