86ad5744fcb4a44b10bdf1b68112b6d54570c64b
fummicc1 <fumiya.tennis1@gmail.com>
2021-10-10 09:34:51 +0900

    done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as dotenv from 'dotenv';
+import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+import * as docs from '../docs/api';
+import * as config from './config/config';
+
 // .envから環境変数を読み込み
 dotenv.config();
 
-const helloWorld = () => 'Hello World';
-console.log(helloWorld());
+const app = express();
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs.document));
+app.listen(config.PORT);
