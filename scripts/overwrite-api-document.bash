796c01f0b148b9c2eadcaace67c17b86b378e84d
fummicc1 <fumiya.tennis1@gmail.com>
2021-10-11 10:02:24 +0900

    fix

--- scripts/overwrite-api-document.bash
+++ scripts/overwrite-api-document.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 BASE_PATH=./src/routes/docs-base.ts
 INPUT_PATH=./src/routes/docs.json
 OUTPUT_PATH=./src/routes/docs.ts
@@ -6,3 +8,4 @@ OUTPUT_PATH=./src/routes/docs.ts
 cp $BASE_PATH $OUTPUT_PATH
 
 sed -i "" -e '1r ./src/routes/docs.json' $OUTPUT_PATH
+echo "Success updating document"
