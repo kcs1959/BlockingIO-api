afade49418a01a805ee4ae15d65d701fab06b64e
fastriver <hykwyuk0125@gmail.com>
2021-10-18 21:02:25 +0900

    add comment

--- src/application/model/game.ts
+++ src/application/model/game.ts
@@ -7,7 +7,10 @@ class Game {
     public listOfPlayer: Player[];
     public tagger: Npc;
     public state: GameState;
+
+    /// 盤面の更新を伝えるリスナー
     private updateListener: GameUpdatedListener | null;
+
     constructor(battleField: Field, listOfPlayer: Player[], tagger: Npc) {
         this.battleField = battleField;
         this.listOfPlayer = listOfPlayer;
@@ -17,6 +20,7 @@ class Game {
         console.log(`Game -> ${this.state}`);
     }
 
+    /// ゲームを開始する
     start(): void {
         this.state = 'PendingStart';
         console.log(`Game -> ${this.state}`);
@@ -34,6 +38,7 @@ class Game {
         this.updateListener = null;
     }
 
+    /// ユーザ側から操作を受け付ける関数
     onTryMove(userId: string, direction: Direction): void {
         if (this.state !== 'InGame') {
             console.log('ゲーム中以外での操作は受け付けられません');
