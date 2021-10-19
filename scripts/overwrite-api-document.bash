a8b44536add6ecc8967bdd975360c3d8e4a72275
akapochi <57896268+akapochi@users.noreply.github.com>
2021-10-19 23:51:20 +0900

    jsonのファイル名を変更 (#51)
    * jsonのファイル名を変更
    
    * docs.jsonとなっていた他の部分も書き換えた
    
    * npm run formatを実行した
    
    Co-authored-by: akapochi <redstar.ihsohaka@gmail.com>

--- scripts/overwrite-api-document.bash
+++ scripts/overwrite-api-document.bash
@@ -2,10 +2,10 @@
 set -e
 
 BASE_PATH=./src/routes/docsBase.ts
-INPUT_PATH=./src/routes/docs.json
+INPUT_PATH=./src/routes/docsJSON.json
 OUTPUT_PATH=./src/routes/docs.ts
 
 cp $BASE_PATH $OUTPUT_PATH
 
-sed -i "" -e '1r ./src/routes/docs.json' $OUTPUT_PATH
+sed -i -e '1r ./src/routes/docsJSON.json' $OUTPUT_PATH
 echo "Success updating document"
