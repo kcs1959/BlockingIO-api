84c45877e83cf944f2b9498da2ba10cbcafb50b2
速川湧気 <hykwyuk0125@gmail.com>
2021-10-20 21:56:59 +0900

    room state

--- src/application/services/userService.ts
+++ src/application/services/userService.ts
@@ -1,8 +1,5 @@
 import { ISocketIOController } from '../../infrastructure/socketIOController';
-import {
-    addNewRoomMemberEvent,
-    fulfillRoomEvent,
-} from '../../routes/socketEvents';
+import { roomStateEvent } from '../../routes/socketEvents';
 import { IUserRepository } from '../repositories/userRepository';
 import { IRoomRepository } from '../repositories/roomRepository';
 import { Room } from '../model/room';
@@ -38,9 +35,8 @@ class UserService implements IUserService {
             console.error('ユーザーがいません');
             throw 'ユーザーがいません';
         }
-        const roomName = 'ルーム' + this.socketController.numberOfRooms();
+        const roomName = await this.socketController.createRoom(socket);
         const room = new Room([user], roomName);
-        await this.socketController.createRoom(roomName, socket);
         this.roomRepository.saveRoom(room);
         return room;
     }
@@ -55,50 +51,30 @@ class UserService implements IUserService {
         if (!user) {
             throw 'ユーザーが存在しません';
         }
-        const room = this.roomRepository.getRoomFromUser(user);
-        if (room) {
+        if (this.roomRepository.getRoomFromUser(user)) {
             throw '既にこのユーザーはルームに入っています';
         }
-        const roomName = this.socketController.getAvailableRoomName();
-        console.info(roomName);
+        let room = this.roomRepository.getAvailableRoom();
         try {
-            if (roomName) {
+            if (room) {
                 // 利用可能な部屋がある場合
                 console.log('利用可能な部屋あり');
-                await this.socketController.joinRoom(roomName, socket);
-                const room = this.roomRepository.getRoomFromName(roomName);
-                if (!room) {
-                    throw `${roomName}という名前のルームが存在しません`;
-                }
-                room.assignedUsers.push(user);
+                await this.socketController.joinRoom(room.roomname, socket);
+                room.addUser(user);
                 this.roomRepository.saveRoom(room);
             } else {
                 // 利用可能な部屋がない場合
                 console.log('利用可能な部屋なし');
                 console.log('ルームの新規作成');
-                await this.createRoom(socketId);
+                room = await this.createRoom(socketId);
             }
-            const room = this.roomRepository.getRoomFromUser(user);
-            if (!room) {
-                throw 'ルームが存在しません';
-            }
+
             this.socketController.sendToRoom(
                 null,
                 room.roomname,
-                addNewRoomMemberEvent,
-                [room]
+                roomStateEvent,
+                room
             );
-
-            const fulfill = room.assignedUsers.length == 2;
-            console.log('fulfill', fulfill);
-            if (fulfill) {
-                this.socketController.sendToRoom(
-                    null,
-                    room.roomname,
-                    fulfillRoomEvent,
-                    [room]
-                );
-            }
 
             return room;
         } catch (err) {
@@ -121,21 +97,20 @@ class UserService implements IUserService {
             room.currentGame.terminate();
             room.currentGame = null;
         }
-        room.assignedUsers = room.assignedUsers.filter(
-            (u) => u.socketId != socketId
-        );
+        room.removeUser(user);
         console.log(`${socketId}のユーザを${room.roomname}から削除しました`);
         console.log(room);
-        const socketRoom = this.socketController.findRoom(room.roomname);
-        if (!socketRoom) {
-            throw 'ソケットルームが見つかりません';
-        }
-        socketRoom.sockets = socketRoom.sockets.filter((s) => s.id != socketId);
-        socketRoom.fulfill = false;
-        if (socketRoom.sockets.length == 0) {
+        if (room.state === 'Empty') {
             console.log(`ルーム: ${room.roomname}を破棄します`);
             await this.socketController.removeRoom(room.roomname);
             this.roomRepository.deleteRoom(room);
+        } else {
+            this.socketController.sendToRoom(
+                null,
+                room.roomname,
+                roomStateEvent,
+                room
+            );
         }
     }
 }
