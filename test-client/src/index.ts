b2b7d5215f1a6f998b06935b06d3d6671a40fe23
fummicc1 <fumiya.tennis1@gmail.com>
2021-10-24 00:57:53 +0900

    :bug: Fix

--- test-client/src/index.ts
+++ test-client/src/index.ts
@@ -15,40 +15,12 @@ if (fieldArea) {
     fieldArea.innerHTML = Array(33).join(Array(33).join('０') + '<br>');
 }
 
-document.body.addEventListener('keydown', (event) => {
-    if (event.key === 'w') {
-        console.log('w');
-        socket.emit('move', 'up');
-    } else if (event.key === 'a') {
-        console.log('a');
-        socket.emit('move', 'left');
-    } else if (event.key === 's') {
-        console.log('s');
-        socket.emit('move', 'down');
-    } else if (event.key === 'd') {
-        console.log('d');
-        socket.emit('move', 'right');
-    }
-});
-
-interface FieldData {
-    squares: string[][];
-}
-
 socket.on('connect', (): void => {
     console.log('connect');
 });
 
 socket.on('disconnect', (): void => {
     console.log('disconnect');
-});
-
-socket.on('field', (field: FieldData) => {
-    if (fieldArea) {
-        fieldArea.innerHTML = field.squares
-            .map((row) => row.join(''))
-            .join('<br>');
-    }
 });
 
 socket.on(fulfillRoomEvent.name, (room) => {
