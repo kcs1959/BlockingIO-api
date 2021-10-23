bdedeb4bf45485f3e558c97ae61b498630c1bc37
fummicc1 <fumiya.tennis1@gmail.com>
2021-10-24 00:45:01 +0900

    :bug: Fix conflict

--- migration/migration.sql
+++ migration/migration.sql
@@ -1,7 +1,6 @@
 CREATE TABLE Users (       
     id TEXT,
-    user_name TEXT NOT NULL,
-    password TEXT NOT NULL,
+    name TEXT NOT NULL,
     point INTEGER NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
--- src/application/model/user.ts
+++ src/application/model/user.ts
@@ -5,14 +5,19 @@ export class User {
     public point: number;
     public uid: string;
     public socketId: string | null;
-    constructor(name: string) {
+    constructor(
+        name: string,
+        point: number,
+        uid: string | null,
+        socketId: string | null
+    ) {
         this.name = name;
-        this.point = 0;
-        this.uid = uuidv4();
-        this.socketId = null;
+        this.point = point;
+        this.uid = uid || uuidv4();
+        this.socketId = socketId;
     }
 
     static generateUnknownUser(): User {
-        return new User('名無しユーザー');
+        return new User('名無しユーザー', 0, null, null);
     }
 }
